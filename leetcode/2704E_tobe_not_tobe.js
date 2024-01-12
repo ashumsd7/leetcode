cda99c4e43f2833753c1ab4a1c1df0f5acffb4aa
Ashutosh Anand Tiwari <ashutosh.tiwari@codemonk.in>
2024-01-12 23:40:00 +0530

    solve : 2704

---
+++ leetcode/2704E_tobe_not_tobe.js
@@ -0,0 +1,24 @@
+
+var expect = function(val) {
+    return {
+        toBe : (val2)=>{
+            if(val===val2)
+            return true
+            else
+            throw new Error("Not Equal");
+
+
+        },
+         notToBe: (val2)=>{
+            if(val!==val2)
+            return true
+            else
+            throw new Error("Equal");
+
+
+        }
+    }
+};
+
+ expect(5).toBe(5); // true
+expect(5).notToBe(5); // throws "Equal"
