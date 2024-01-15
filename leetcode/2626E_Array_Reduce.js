9c73d8b20a591bf34ba20e26ab1e077c353965c2
Ashutosh Anand Tiwari <ashutosh.tiwari@codemonk.in>
2024-01-16 01:01:16 +0530

    solve array

--- leetcode/2626E_Array_Reduce.js
+++ leetcode/2626E_Array_Reduce.js
@@ -1,6 +1,5 @@
 var reduce = function (nums, fn, init) {
     if (!nums.length) return init
-    // let res = init
     for (i = 0; i < nums.length; i++)
     init = fn(init, nums[i])
     return init
