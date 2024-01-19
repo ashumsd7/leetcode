a7c8f2917fbe94d41eb181b693de4af57fb53e56
Ashutosh Anand Tiwari <ashutosh.tiwari@codemonk.in>
2024-01-19 21:06:46 +0530

    solve: 125

---
+++ leetcode/125E_ValidPalindrome.js
@@ -0,0 +1,17 @@
+var isPalindrome = function (s) {
+
+    s = s.replace(/[^a-z0-9]/gi, '').toLowerCase()
+    let temp = s
+    s=s.split('').reverse().join('')
+     return temp=== s
+};
+
+
+
+
+
+
+let input = "A man, a plan, a canal: Panama"
+
+
+console.log('isPalindrome', isPalindrome(input))
